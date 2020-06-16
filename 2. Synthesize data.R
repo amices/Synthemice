2a76622e8ee95368f3f625038528a040c4e25ea7
Thom Volker <t.b.volker@uu.nl>
2020-06-16 12:02:50 +0200

    Partitioning of data

--- 2. Synthesize data.R
+++ 2. Synthesize data.R
@@ -1,1 +1,24 @@
 # Create synthetic versions of the data
+
+# Obtain the previously synthesized data
+source("1. Create data.R")
+
+##### This is all very preliminary.
+
+syns <- lapply(part, function(x) syn(x[[1]]))
+
+syns$group1$syn
+
+part %>%
+  map(., ~ syn(.))
+
+group1 <- part$group1[[1]]
+
+install.packages("synthpop")
+
+syn1 <- syn(group1)
+
+syn1$syn
+summary(syn1$syn)
+summary(group1)
+compare(syn1, group1)
