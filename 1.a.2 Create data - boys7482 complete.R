be4f27aef2ef1c154cb25126438c1b8cfa0d238a
Thom Volker <t.b.volker@uu.nl>
2020-07-16 09:24:34 +0200

    Full boys data added

---
+++ 1.a.2 Create data - boys7482 complete.R
@@ -0,0 +1,35 @@
+###############################################################################
+## Create data sets to work with, we will work with the boys dataset that is ##
+## build-in in mice.                                                         ##
+###############################################################################
+
+
+###############################################################################
+## Load packages and set seed                                                ##
+###############################################################################
+
+library(mice)   ## mice for the boys data and the imputations
+library(modelr) ## modelr to do the partitioning   
+
+set.seed(123)   # seed for reproducibility of the imputations and partitioning
+
+###############################################################################
+## obtain and impute the boys dataset, create the completed boys data, and   ##
+## partition the completed dataset                                           ##
+###############################################################################
+
+meth <- make.method(AGD::boys7482)
+pred <- make.predictorMatrix(boys)
+meth["bmi"] <- "~ I(wgt / (hgt/100)^2)"
+
+pred[c("wgt", "hgt"), "bmi"] <- 0
+
+imp7482 <- mice(AGD::boys7482, meth = meth, pred = pred,      ## Impute the boys dataset by means of a single
+            m = 1, printFlag = F)                    ## imputation, and fill in the imputed data into
+boyscomp7482 <- complete(imp7482)                        ## the dataset containing missing values.
+
+
+ngroups <- 5                                         ## Specify five groups, and
+partitions <- rep(1/ngroups,ngroups)                 ## specify five equal fractions
+names(partitions) <- paste0("d", 1:ngroups)          ## give names to the partitionings and
+part <- resample_partition(boyscomp7482, partitions) ## partition the data in five equal parts
